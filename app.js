4633ac5646db579e06a44b01844c1dac5c34e8a4
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2025-01-16 16:57:21 -0300

    TERMINADO

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const app = express();
+const PORT = 4000;
+
+const url = "https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap";
+const baseUrl = "https://es.wikipedia.org/"
+const enlacesRaperos = [];
+const detallesRaperos = [];
+
+app.get("/", (req, res) => {
+    const resp = axios(url).then((response) => {
+        if(response.status === 200){
+            const html = response.data
+            /* console.log(html) */
+            const $ = cheerio.load(html) 
+            const title = $("title").text()  
+
+            $(".mw-category-group a").each((index, element) => {
+                const enlace = $(element).attr("href");
+                const linkCompleto = baseUrl + enlace
+                enlacesRaperos.push(linkCompleto);
+            });
+        
+            /* console.log(enlacesRaperos); */
+            enlacesRaperos.splice(0, 8);
+
+            res.send(`
+                <h1>${title}</h1>
+                <a href="/raperos"><p>Raperos</p></a>
+                `)
+        }
+    })
+});
+
+app.get("/raperos", async (req, res) => {
+    try{
+        if(!enlacesRaperos){
+            return res.status(404).send("Error 404: No se ha Encontrado Enlace Disponible")
+        }
+        await Promise.all(
+            enlacesRaperos.map(async (link) => {
+                const response = await axios(link);
+                const html = response.data;
+                const $ = cheerio.load(html);
+
+                const nombre = $(".mw-page-title-main").text();
+                const imagen = $(".infobox img").attr("src"); // Selector mejorado
+                const texto = $("p").first().text();
+
+                detallesRaperos.push({ nombre, imagen, texto});
+            })
+        );
+
+        console.log(detallesRaperos); 
+        
+        let htmlRaperos = `<h1>Detalles de Raperos</h1>`;
+        detallesRaperos.forEach(rapero => {
+            htmlContent += `
+                <div class="rapero">
+                    <h2>${rapero.nombre}</h2>
+                    <img src="${"http:" + rapero.imagen}" alt="${rapero.nombre}" style="max-width:200px;">
+                    <p>${rapero.texto}</p>
+                </div>`;
+        });
+
+        res.send(htmlRaperos);
+    }
+    catch(error){
+        console.error(error, "Error en la Solicitud")
+        res.status(500).send("Error 500 - Error en la Terminal");
+    }
+});
+
+app.listen (PORT, () => {
+    console.log(`Server Listening On Port http://localhost${PORT}`);
+})
